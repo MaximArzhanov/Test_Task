12eaca03dad96ad1e96873fa9ded0e3785a82fd3
MaximArzhanov <arzhanov93@yandex.ru>
2022-05-17 12:02:24 +0500

    feat: Реализована маска для поля с номером телефона

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ const buttonClose = document.querySelector('.popup__icon-close');
 const overlay = document.querySelector('.overlay');
 const popup = document.querySelector('.popup');
 
+const phoneField = document.querySelector('#phone');
+
 // Добавление обработчика события при открытии popup
 function setEventListeners() {
   overlay.addEventListener('click', closePopup);
@@ -29,3 +31,27 @@ function closePopup(evt) {
 
 buttonOpen.addEventListener('click', openPopup);
 
+function changePhone() {
+  var newPhoneValue = '';
+  // Введённый символ
+  const enteredCharacter = parseInt(phoneField.value[phoneField.selectionStart - 1]);
+  // Проверка на число. Если ввдено число, то нижнее подчёркивание заменяется на число
+  if (Number.isInteger(enteredCharacter)) { newPhoneValue = phone.value.replace('_', ''); }
+  // Если введено не число, то символ удаляется
+  else { newPhoneValue = phoneField.value.slice(0, phoneField.selectionStart - 1) + phoneField.value.slice(phoneField.selectionStart); }
+  // Запись значение в инпут
+  phoneField.value = newPhoneValue;
+  // Выставление курсора на следующую позицию
+  phoneField.selectionStart = phoneField.selectionEnd = phoneField.value.indexOf('_');
+}
+
+// Устанавливает курсор перед нижним подчёркиванием
+function setCursor() {
+  const cursorPlace = phoneField.value.indexOf('_');
+  setTimeout(() => {
+    phoneField.selectionStart = phoneField.selectionEnd = cursorPlace;
+  }, 0);
+}
+
+phoneField.addEventListener('focus', setCursor);
+phoneField.addEventListener('input', changePhone);
